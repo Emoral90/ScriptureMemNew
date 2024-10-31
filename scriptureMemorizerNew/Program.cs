95f2d33e7dbdd605b29d28e18fa290777d2a8e84
Erick <moralerick90@gmail.com>
2024-10-31 09:31:31 -0700

    modified program main

--- scriptureMemorizerNew/Program.cs
+++ scriptureMemorizerNew/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        var reference = new Reference("John", 3, 16);
+        var scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide words, or type 'quit' to exit.");
+            var input = Console.ReadLine();
+            
+            if (input.ToLower() == "quit" || scripture.IsCompletelyHidden())
+                break;
+
+            scripture.HideRandomWords(3);  // Hide 3 words at a time
+        }
+
+        Console.WriteLine("All words are hidden. Program has ended.");
+    }
+}
